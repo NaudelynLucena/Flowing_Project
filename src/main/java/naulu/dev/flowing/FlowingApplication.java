3afa4afe297328727a590631ee9affb4f5c89e7b
Naudelyn Lucena <you@example.com>
2024-12-08 00:05:48 +0100

    Initialising repository

---
+++ src/main/java/naulu/dev/flowing/FlowingApplication.java
@@ -0,0 +1,13 @@
+package naulu.dev.flowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/naulu/dev/flowing/FlowingApplicationTests.java
@@ -0,0 +1,13 @@
+package naulu.dev.flowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
