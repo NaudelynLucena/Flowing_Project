8feb5e01a4f3f860a8de414e01eea9113eb81f4b
Naudelyn Lucena <you@example.com>
2024-12-08 16:59:56 +0100

    Attribute added and missing getters and setters added

--- src/main/java/dev/naulu/flowing/model/User.java
+++ src/main/java/dev/naulu/flowing/model/User.java
@@ -25,13 +25,22 @@ public class User implements UserDetails {
 
     public User() {}
 
-    public User(String email, String password) {
+    public User(String email, String password, String name) {
         this.email = email;
         this.password = password;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
